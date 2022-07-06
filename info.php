c60b6e4a81b774217fa8f036bc9a81ae97a4d357
Nelson ANDRADE <nelsonandrade7771@gmail.com>
2022-07-06 18:00:41 +0200

    Première commit
    upload de s tous les fichiers do site

---
+++ info.php
@@ -0,0 +1,5 @@
+
+
+<a class="pages" href="voitures.php" target="_self" title="Voitures"><img src="img/car.ico" alt="voitures" ></a>
+<a class="pages" href="login.php" target="_self" title="Contact"><img src="img/telef.png" alt="contact"></a>
+<a class="pages" href="" target="_self" title="Nous Trouvez"><img src="img/gps.ico" alt="gps"></a>
